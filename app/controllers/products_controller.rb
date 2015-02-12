9c7f900b909deac547436ac76f3c632c399ba941
A.Krulov <a.krulov@gmail.com>
2015-02-13 01:37:13 +0300

    add method for change order, add generate order string, add default price

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,29 +1,46 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_filter :set_order
+  def set_order
+    @orders = ['price']
+  end
 
   # GET /products
   # GET /products.json
   def index
     @products = Product.all
+    @orders = ['price']
   end
 
   def search
     #@producthash = Hash.new
     #@table = Product.arel_table
-    orders = ['price']
     @stext = params[:stext]
+    #@orders = ['price']
     stexthash = @stext.split(' ')
     if stexthash.empty? then
       @products = Product.all
     else
       stexthash.map! {|textpce| textpce = "%"+textpce+"%" }
-      #evstring = 'Product.where{(productname.like_all stexthash) | (productarticul.like_any stexthash)}'
-      #orders.each {|ordtxt| evstring = evstring + tdtxt + "{.asc}"}
-      @products = Product.where{(productname.like_all stexthash) | (productarticul.like_any stexthash)}.order{price.asc}.order{productname.asc}
+      evstring = 'Product.where{(productname.like_all stexthash) | (productarticul.like_any stexthash)}'
+      @orders.each {|ordtxt| evstring = evstring + ".order{" + ordtxt + "}"}
+      @products = eval (evstring)
+      #@products = Product.where{(productname.like_all stexthash) | (productarticul.like_any stexthash)}.order{price.asc}.order{productname.asc}
     end
     render :index
   end
 
+  def setorder
+    ord = params[:setorder]
+    if @orders.include?(ord) then
+      @orders.map! {|order| order == ord ? ord+'.desc' : order}
+    elsif @orders.include?(ord+'.desc')
+      @orders.delete(ord+'.desc')
+    else
+      @orders.push(ord)
+    end
+    render :index
+  end
   # GET /products/1
   # GET /products/1.json
   # def show
