f7d3a6a474e857501f38d49925eb720e8b73d4b4
A.Krulov <a.krulov@gmail.com>
2015-04-27 06:22:30 +0300

    done relation in order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ActiveRecord::Base
-  belongs_to :employee
+  belongs_to :manager, class_name: "Employee", foreign_key: "manager_id"
+  belongs_to :doner, class_name: "Employee", foreign_key: "doner_id"
   has_many :orderparts
 end
