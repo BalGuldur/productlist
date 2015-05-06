c18318dead207fca117529878520ec5826b34a10
A.Krulov <a.krulov@gmail.com>
2015-05-06 21:00:19 +0300

    ltl change relation in orderparts and check they

--- app/models/orderpart.rb
+++ app/models/orderpart.rb
@@ -4,6 +4,7 @@ class Orderpart < ActiveRecord::Base
   belongs_to :panswtime, class_name: "Ourtime"
   belongs_to :rezpricetype, class_name: "Moneytype"
   belongs_to :reztime, class_name: "Ourtime"
+  belongs_to :pshiptime, class_name: "Ourtime"
   belongs_to :order
 
 end
