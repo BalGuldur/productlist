04c44ea9d8ab94f413118286d321c87b05d39b77
A.Krulov <a.krulov@gmail.com>
2015-05-06 20:39:30 +0300

    add relation in orderparts

--- app/models/orderpart.rb
+++ app/models/orderpart.rb
@@ -1,2 +1,8 @@
 class Orderpart < ActiveRecord::Base
+  belongs_to :doner, class_name: "Employee"
+  belongs_to :state, class_name: "Orderstate"
+  belongs_to :panswtime, class_name: "Ourtime"
+  belongs_to :rezpricetype, class_name: "Moneytype"
+  belongs_to :reztime, class_name: "Ourtime"
+
 end
