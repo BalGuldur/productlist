06c790689571ae0f7ff2beecdf43839e68234c6d
A.Krulov <a.krulov@gmail.com>
2015-05-13 00:25:33 +0300

    ltl chg

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'products#index'
+  root 'orders#index'
   # root 'welcome#index'
 
   # Example of regular route:
